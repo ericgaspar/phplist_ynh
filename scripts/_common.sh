47bac96609b8a310772cf03613b2b68271cc27b8
ericgaspar <junk.eg@free.fr>
2022-03-04 15:51:11 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="8.0"
+YNH_PHP_VERSION="7.4"
 
 pkg_dependencies="php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-mbstring"
 
